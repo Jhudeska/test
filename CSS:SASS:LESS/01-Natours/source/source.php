a27cf0e175b5bba205b543177fa991a7cbd8ab3d
Jhudeska Ignacia <jvsignacia@live.nl>
2018-07-09 09:18:51 +0200

    Natours Website part 2

--- CSS:SASS:LESS/01-Natours/source/source.php
+++ CSS:SASS:LESS/01-Natours/source/source.php
@@ -1,7 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jhudeska
- * Date: 03/07/2018
- * Time: 15:07
- */
+
+
+#extra:
+#sass globally https://stackoverflow.com/questions/39330727/node-sass-command-not-found-when-compiling
+#Command to compile sass  to css compiler:  #npm run compile:sass
--- Private/00-Realtime Geolocation/source/source.php
+++ Private/00-Realtime Geolocation/source/source.php
@@ -1,3 +1,7 @@
 <?php
 
 # Extra links
+# https://www.pubnub.com/tutorials/javascript/mapping-javascript-tracking/
+# https://www.youtube.com/watch?v=xJubVipcHrM
+
+
